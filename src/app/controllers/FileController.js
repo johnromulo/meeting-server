98f2bee3408eba9ce274083a2f13cb0079ead6dc
John Rômulo <johnromulo0801987@gmail.com>
2019-08-17 19:40:32 -0300

    resize img

--- src/app/controllers/FileController.js
+++ src/app/controllers/FileController.js
@@ -1,3 +1,6 @@
+import sharp from 'sharp';
+import { resolve } from 'path';
+import sizeOf from 'image-size';
 import File from '../models/File';
 
 class FileController {
@@ -9,7 +12,18 @@ class FileController {
       path,
     });
 
-    return res.json(file);
+    const tmp = resolve(__dirname, '..', '..', '..', 'tmp', 'uploads', path);
+
+    const size = sizeOf(tmp);
+
+    await sharp(tmp)
+      .rotate()
+      .resize(Math.ceil(size.width * 0.1), Math.ceil(size.height * 0.1))
+      .toFile(
+        resolve(__dirname, '..', '..', '..', 'tmp', 'uploads', `@small-${path}`)
+      );
+
+    return res.json({ file, size });
   }
 }
 
